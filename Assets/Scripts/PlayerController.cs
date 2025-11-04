b7fbfe3568c315bbad0f8d5215527a9c3d293f0c
Omar Gad <149539798+omargad-sys@users.noreply.github.com>
2025-11-03 20:25:55 -0500

    added jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,18 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 0;
+    public float jumpForce = 8.0f;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     public GameObject Door1;
     public GameObject Door2;
     public GameObject Door3;
-        public GameObject Door4;
-
-
+    public GameObject Door4;
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
+    private bool grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,13 +26,25 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText();
         winTextObject.SetActive(false);
+        grounded = true; 
     }
+
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            grounded = false;
+        }
+    }
+
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
@@ -85,12 +97,24 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
-        {
-            // Destroy the current object
+        {            // Destroy the current object
             Destroy(gameObject);
             // Update the winText to display "You Lose!"
             winTextObject.gameObject.SetActive(true);
             winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
         }
     }
 }
